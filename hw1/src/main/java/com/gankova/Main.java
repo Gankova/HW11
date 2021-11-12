88c847538a01ea3d85dce3bb9c13ad30b9e7fa1d
Gankova <80777045+Gankova@users.noreply.github.com>
2021-11-12 21:13:45 +0300

    Add files via upload

---
+++ hw1/src/main/java/com/gankova/Main.java
@@ -0,0 +1,32 @@
+package com.gankova;
+
+import com.gankova.service.ArticleService;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        validate(args);
+        String method = args[0];
+        Long id = Long.parseLong(args[1]);
+        ArticleService articleService = new ArticleService();
+
+        if (method.equals("GET")) {
+            articleService.getArticle(method, id);
+        }
+
+    }
+
+    public static void validate(String[] args) {
+        if (args.length != 2){
+            throw new IllegalArgumentException("Icorrect params");
+        }
+        if (args[0].equals("GET") || args[0].equals("POST")) {
+            if (Long.parseLong(args[1]) < 1) {
+                throw new IllegalArgumentException("Icorrect params");
+            }
+        } else {
+            throw new IllegalArgumentException("Icorrect params");
+        }
+    }
+}
